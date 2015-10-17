44fccba4e4ccd7c350e020ffd5d4bca4c8ea24f9
–ê–ª–µ–Ω–∞ <Egor@macbook-pro-egor.local>
2015-10-17 13:47:17 +0300

    Initial Commit

---
+++ AuctionExport/SearchFilter.swift
@@ -0,0 +1,23 @@
+//
+//  SearchFilter.swift
+//  AuctionExport
+//
+//  Created by Егор Сидоренко on 10/17/15.
+//  Copyright © 2015 Егор Сидоренко. All rights reserved.
+//
+
+//import Foundation
+
+import UIKit
+
+class SearchFilterController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
